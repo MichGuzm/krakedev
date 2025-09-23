9097d2c6fc312a794ae5ed379b946e52666099ec
MichGuzm <michguzman2196@gmail.com>
2025-09-22 21:29:11 -0500

    recperarInt y recuperarFloat ok

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
 }
 
 recuperarTexto=function(idComponente){
@@ -11,3 +13,14 @@ recuperarTexto=function(idComponente){
     return valorIngresado;
 }
 
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFlotante=parseFloat(valorCaja);
+    return valorFlotante;
+}
