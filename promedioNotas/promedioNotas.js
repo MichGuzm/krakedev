492ae4e9c1478e493c5050e9b460f1f602a7885e
MichGuzm <michguzman2196@gmail.com>
2025-09-24 17:21:52 -0500

    se calcula el promedio con exito

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,10 @@
+calcularPromedioNotas=function(){
+    let n1=recuperarFlotante("nota1");
+    let n2=recuperarFlotante("nota2");
+    let n3=recuperarFlotante("nota3");
+    let promedio=calcularPromedio(n1,n2,n3);
+    let promedioFormateado=promedio
+    let cmpPromedio=document.getElementById("lblPromedio");
+    cmpPromedio.innerText=promedioFormateado.toFixed(2);
+
+}
