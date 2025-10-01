a3b40391a6f13166544e37f13cd89a22602380cb
MichGuzm <michguzman2196@gmail.com>
2025-09-30 21:48:00 -0500

    agregada funcion

--- puntoDeVentaV2/puntoDeVenta.js
+++ puntoDeVentaV2/puntoDeVenta.js
@@ -5,7 +5,7 @@ calcularValorTotal = function () {
     let porcentajeDescuento=recuperarInt("txtPorcentajeDescuento");
 
     let valorSubtotal=calcularSubtotal(precioProducto,cantidad);
-    let valorDescuento=calcularDescuento(valorSubtotal,porcentajeDescuento);
+    let valorDescuento=calcularDescuentoPorVolumen(valorSubtotal,cantidad);
     let valorSinIva=valorSubtotal-valorDescuento;
     let valorIVA=calcularIva(valorSinIva);
     let valorTotal=calcularTotal(valorSubtotal,valorDescuento,valorIVA);
@@ -29,4 +29,19 @@ limpiar = function () {
     mostrarTexto("lblValorIVA","0.0");
     mostrarTexto("lblTotal","0.0");
     mostrarTexto("lblResumen","");
+}
+
+calcularDescuentoPorVolumen=function(subtotal,cantidad){
+     let porcentajeDescuento = 0;
+    if (cantidad < 3) {
+        porcentajeDescuento = 0;
+    } else if (cantidad >= 3 && cantidad <= 5) {
+        porcentajeDescuento = 0.03;
+    } else if (cantidad >= 6 && cantidad <= 11) {
+        porcentajeDescuento = 0.04; 
+    } else if (cantidad >= 12) {
+        porcentajeDescuento = 0.05; 
+    }
+    let descuento = subtotal * porcentajeDescuento;
+    return descuento;
 }
