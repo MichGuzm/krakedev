134e7f1d41eb68fcf7169cb7db9224f515905289
MichGuzm <michguzman2196@gmail.com>
2025-10-02 16:17:32 -0500

    todo funciona

--- juego/juego.js
+++ juego/juego.js
@@ -1,3 +1,6 @@
+let puntosUsuario=0;
+let puntosComputador=0;
+
 jugar=function(seleccionado){
     let elemento=generarElemento();
     let ruta=generarRuta(elemento);
@@ -5,10 +8,32 @@ jugar=function(seleccionado){
     let resultado=determinarGanador(elemento,seleccionado);
 
     if(resultado==0){
-        mostrarTexto("lblResultado","EMPATE");
+        mostrarTexto("lblResultadoParcial","EMPATE");
     } else if(resultado==1){
-        mostrarTexto("lblResultado","PERDISTE LA PARTIDA");
+        puntosComputador++;
+        mostrarTexto("lblResultadoParcial","PERDISTE LA PARTIDA");
     }else if(resultado==2){
-        mostrarTexto("lblResultado","GANASTE LA PARTIDA");
+        puntosUsuario++;
+        mostrarTexto("lblResultadoParcial","GANASTE LA PARTIDA");
+    }
+     mostrarTexto("lblUsuario",puntosUsuario);
+     mostrarTexto("lblCPU",puntosComputador);
+
+    if(puntosUsuario==5){
+    mostrarTexto("lblResultadoTotal","HAS GANADO EL JUEGO");
+    mostrarTexto("lblResultadoParcial","");
+    }else if(puntosComputador==5){
+    mostrarTexto("lblResultadoParcial","");
+    mostrarTexto("lblResultadoTotal","EL COMPUTADOR TE HA VENCIDO");
     }
 }
+
+limpiar=function(){
+    puntosUsuario=0;
+    puntosComputador=0;
+    mostrarTexto("lblResultadoTotal","");
+    mostrarTexto("lblUsuario",0);
+    mostrarTexto("lblCPU",0);
+    mostrarImagen("img","");
+}
+
