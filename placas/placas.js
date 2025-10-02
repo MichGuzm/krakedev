9d91a35ad5d19d5bf0f77b2b703f4025752df294
MichGuzm <michguzman2196@gmail.com>
2025-10-01 22:31:30 -0500

    se a;adio provincias

--- placas/placas.js
+++ placas/placas.js
@@ -1,10 +1,19 @@
 validarPlaca=function(){
     let valorIngresado=recuperarTexto("lblPlaca");
     let erroresEstructura=validarEstructura(valorIngresado);
+    
     if(erroresEstructura==null){
+        let provincia=obtenerProvincia(valorIngresado);
+        if(provincia!==null){
         mostrarTexto("lblSmsEstructura","ESTRUCTURA VALIDA");
+        mostrarTexto("lblProvincia",provincia);
+        }else{
+            mostrarTexto("lblSmsEstructura","ESTRUCTURA VALIDA");
+            mostrarTexto("lblProvincia","PROVINCIA INCORRECTA");
+        }
     }else{
         mostrarTexto("lblSmsEstructura","ESTRUCTURA INCORRECTA");
         mostrarTexto("lblErrores",erroresEstructura);
     }
 }
+
--- placas/serviciosPlacas.js
+++ placas/serviciosPlacas.js
@@ -1,3 +1,56 @@
+obtenerProvincia=function(placa){
+    let primeraLetra=placa[0];
+    if (primeraLetra=="A") {
+        return "Azuay";
+    } else if (primeraLetra=="B") {
+        return "Bolívar";
+    } else if (primeraLetra=="U") {
+        return "Cañar";
+    } else if (primeraLetra=="C") {
+        return "Carchi";
+    } else if (primeraLetra=="X") {
+        return "Cotopaxi";
+    } else if (primeraLetra=="H") {
+        return "Chimborazo";
+    } else if (primeraLetra=="O") {
+        return "El Oro";
+    } else if (primeraLetra=="E") {
+        return "Esmeraldas";
+    } else if (primeraLetra=="W") {
+        return "Galápagos";
+    } else if (primeraLetra=="G") {
+        return "Guayas";
+    } else if (primeraLetra=="I") {
+        return "Imbabura";
+    } else if (primeraLetra=="L") {
+        return "Loja";
+    } else if (primeraLetra=="R") {
+        return "Los Ríos";
+    } else if (primeraLetra=="M") {
+        return "Manabí";
+    } else if (primeraLetra=="V") {
+        return "Morona Santiago";
+    } else if (primeraLetra=="N") {
+        return "Napo";
+    } else if (primeraLetra=="S") {
+        return "Pastaza";
+    } else if (primeraLetra=="P") {
+        return "Pichincha";
+    } else if (primeraLetra=="Q") {
+        return "Sucumbíos";
+    } else if (primeraLetra=="K") {
+        return "Zamora Chinchipe";
+    } else if (primeraLetra=="T") {
+        return "Tungurahua";
+    } else if (primeraLetra=="Z") {
+        return "Zamora Chinchipe";
+    } else if (primeraLetra=="Y") {
+        return "Santa Elena";
+    } else {
+        return null;
+    }
+}
+
 validarEstructura = function (placa) {
     let error = null;
     if (placa.length !== 7 && placa.length !== 8) {
@@ -30,3 +83,4 @@ validarEstructura = function (placa) {
 
     return error;
 }
+
