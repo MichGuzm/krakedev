38e0517f69c4d7aaf73298f40afe0757181c12eb
MichGuzm <michguzman2196@gmail.com>
2025-10-02 22:27:37 -0500

    se corrigio length

---
+++ bucles/ejerciciosCadenas.js
@@ -0,0 +1,19 @@
+ejecutarPrueba1=function(){
+    let mensaje;
+    mensaje=recuperarTexto("txtCadena");
+    recorrerCadena(mensaje);
+}
+
+recorrerCadena=function(cadena){
+    let caracter;
+    
+    for(let posicion=0;posicion<cadena.length;posicion++){
+        caracter=cadena.charAt(posicion);
+        console.log("Caracter "+caracter+" posicion"+posicion);
+    }
+
+    for(let posicion=0;posicion<=cadena.length-1;posicion++){
+        caracter=cadena.charAt(posicion);
+        console.log("CARACTER "+caracter+" posicion"+posicion);
+    }
+}
