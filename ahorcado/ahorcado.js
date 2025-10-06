72b9fe9ff345cebb1c92e7b1046d2d06e94a5555
MichGuzm <michguzman2196@gmail.com>
2025-10-06 16:23:57 -0500

    Paso 1 completo

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -1,3 +1,5 @@
+let palabraSecreta;
+
 esMayuscula=function(caracter){
     let codigoAscii=caracter.charCodeAt(0);
     if(codigoAscii>=65&&codigoAscii<=90){
@@ -6,3 +8,24 @@ esMayuscula=function(caracter){
         return false;
     }
 }
+
+guardarPalabra=function(){
+    let texto=recuperarTexto("txtSecreta");
+
+    let noLength=false;
+    if (texto.length!== 5) {
+        alert("DEBE INGRESAR UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYÚSCULAS");
+        noLength=true;    
+    }
+    let noMayuscula=false;
+    for (let i=0;i<texto.length;i++) {
+        let caracter=texto.charAt(i);
+        if (!esMayuscula(caracter)) {
+            alert("INGRESE UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYUSCULAS");
+            noMayuscula=true;
+        }
+    }
+
+    palabraSecreta=texto;
+}
+
