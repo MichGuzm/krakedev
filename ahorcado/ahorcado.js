c5fedffa0160751eccd04b08c6180071f68f4ea8
MichGuzm <michguzman2196@gmail.com>
2025-10-06 16:48:21 -0500

    paso 2 done

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -1,31 +1,48 @@
 let palabraSecreta;
 
-esMayuscula=function(caracter){
-    let codigoAscii=caracter.charCodeAt(0);
-    if(codigoAscii>=65&&codigoAscii<=90){
+esMayuscula = function (caracter) {
+    let codigoAscii = caracter.charCodeAt(0);
+    if (codigoAscii >= 65 && codigoAscii <= 90) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-guardarPalabra=function(){
-    let texto=recuperarTexto("txtSecreta");
+guardarPalabra = function () {
+    let texto = recuperarTexto("txtSecreta");
 
-    let noLength=false;
-    if (texto.length!== 5) {
+    let noLength = false;
+    if (texto.length !== 5) {
         alert("DEBE INGRESAR UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYÚSCULAS");
-        noLength=true;    
+        noLength = true;
     }
-    let noMayuscula=false;
-    for (let i=0;i<texto.length;i++) {
-        let caracter=texto.charAt(i);
+    let noMayuscula = false;
+    for (let i = 0; i < texto.length; i++) {
+        let caracter = texto.charAt(i);
         if (!esMayuscula(caracter)) {
             alert("INGRESE UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYUSCULAS");
-            noMayuscula=true;
+            noMayuscula = true;
         }
     }
-
-    palabraSecreta=texto;
+    palabraSecreta = texto;
 }
 
+mostrarLetra = function (letra, posicion) {
+        if (posicion==0) {
+            mostrarTexto("div0", letra);
+        }
+        if (posicion==1) {
+            mostrarTexto("div1", letra);
+        }
+        if (posicion==2) {
+            mostrarTexto("div2", letra);
+        }
+        if (posicion==3) {
+            mostrarTexto("div3", letra);
+        }
+        if (posicion==4) {
+            mostrarTexto("div4", letra);
+        }
+    
+}
