984a67a74ed93961031ac7e2075727cc7a60e63a
MichGuzm <michguzman2196@gmail.com>
2025-10-06 18:23:53 -0500

    prueba1

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -1,4 +1,4 @@
-let palabraSecreta;
+let palabraSecreta="";
 
 esMayuscula = function (caracter) {
     let codigoAscii = caracter.charCodeAt(0);
@@ -12,50 +12,66 @@ esMayuscula = function (caracter) {
 guardarPalabra = function () {
     let texto = recuperarTexto("txtSecreta");
 
-    let noLength = false;
-    if (texto.length !== 5) {
+    let noLength=false;
+    if (texto.length!==5) {
         alert("DEBE INGRESAR UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYÚSCULAS");
-        noLength = true;
+        return;
     }
-    let noMayuscula = false;
-    for (let i = 0; i < texto.length; i++) {
-        let caracter = texto.charAt(i);
+    let noMayuscula=false;
+    for (let i=0;i<texto.length;i++) {
+        let caracter=texto.charAt(i);
         if (!esMayuscula(caracter)) {
             alert("INGRESE UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYUSCULAS");
-            noMayuscula = true;
+            return;
         }
     }
-    palabraSecreta = texto;
+    palabraSecreta=texto;
 }
 
 mostrarLetra = function (letra, posicion) {
+    console.log(letra);
         if (posicion==0) {
-            mostrarTexto("div0", letra);
+            mostrarTexto("div0",letra);
         }
         if (posicion==1) {
-            mostrarTexto("div1", letra);
+            mostrarTexto("div1",letra);
         }
         if (posicion==2) {
-            mostrarTexto("div2", letra);
+            mostrarTexto("div2",letra);
         }
         if (posicion==3) {
-            mostrarTexto("div3", letra);
+            mostrarTexto("div3",letra);
         }
         if (posicion==4) {
-            mostrarTexto("div4", letra);
+            mostrarTexto("div4",letra);
         }
     
 }
 
 validar=function(letra){
+
+    console.log(palabraSecreta);
+
     let letrasEncontradas=0;
      
-    for(i=0;i<palabraSecreta.length;i++){
+    for (let i=0;i<palabraSecreta.length;i++) {
         let caracterActual=palabraSecreta.charAt(i);
-    }
-    if(caracterActual==letra){
+    
+        if(caracterActual==letra){
         mostrarLetra(letra,i);
-        letrasEncontradas++;
+        letrasEncontradas=letrasEncontradas+1;
+        }
     }
 }
 
+ingresarLetra=function(){
+    
+    let letra=recuperarTexto("txtLetra");
+    console.log(letra);
+
+    if(esMayuscula(letra)){
+        validar(letra);
+    }else{
+        alert("SOLO SE ACEPTAN MAYUSCULAS");
+    }
+}
