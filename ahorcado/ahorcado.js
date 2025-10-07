8599d45843758e55902c41fb99d7f6ad75082815
MichGuzm <michguzman2196@gmail.com>
2025-10-06 19:33:38 -0500

    paso 7 done

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -69,6 +69,7 @@ validar=function(letra){
     if (letrasEncontradas==0) {
         alert("LA LETRA NO ES PARTE DE LA PALABRA");
         errores=errores+1;
+        mostrarAhorcado();
     }
 }
 
@@ -81,11 +82,34 @@ ingresarLetra=function(){
         intentos=intentos+1;
         validar(letra);
         if(coincidencias>=5){
-            alert("HA GANADO");
+            mostrarImagen("ahorcadoImagen","ganador.gif");
         }else if(intentos>=10){
-            alert("HA PERDIDO");
+            mostrarImagen("ahorcadoImagen","gameOver.gif");
         }
     }else{
         alert("SOLO SE ACEPTAN MAYUSCULAS");
     }
 }
+
+mostrarAhorcado=function(){
+    if (errores==1){
+        mostrarImagen("ahorcadoImagen","Ahorcado_01.png");
+    } else if (errores==2){
+        mostrarImagen("ahorcadoImagen","Ahorcado_02.png");
+    } else if (errores==3){
+        mostrarImagen("ahorcadoImagen","Ahorcado_03.png");
+    } else if (errores==4){
+        mostrarImagen("ahorcadoImagen","Ahorcado_04.png");
+    } else if (errores==5){
+        mostrarImagen("ahorcadoImagen","Ahorcado_05.png");
+    } else if (errores==6){
+        mostrarImagen("ahorcadoImagen","Ahorcado_06.png");
+    } else if (errores==7){
+        mostrarImagen("ahorcadoImagen","Ahorcado_07.png");
+    } else if (errores==8){
+        mostrarImagen("ahorcadoImagen","Ahorcado_08.png");
+    } else if (errores==9){
+        mostrarImagen("ahorcadoImagen","Ahorcado_09.png");
+    }
+}
+
