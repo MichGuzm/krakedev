fb88c1d98e0beac3b307fe83504669b28d35dc99
MichGuzm <michguzman2196@gmail.com>
2025-10-13 08:21:34 -0500

    reto 46 done

--- roles/roles.js
+++ roles/roles.js
@@ -3,3 +3,24 @@ let empleados = [
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
 ]
 
+mostrarEmpleados=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    let contenidoTabla="<table class='tabla-empleados'><tr>"+
+    "<th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th>"+
+    "</tr>";
+    
+    let elementoEmpleado;
+    for(let i=0;i<empleados.length;i++){
+        elementoEmpleado=empleados[i];
+        contenidoTabla+="<tr><td>"+elementoEmpleado.cedula+"</td>" +
+                         "<td>"+elementoEmpleado.nombre+"</td>" +
+                         "<td>"+elementoEmpleado.apellido+"</td>" +
+                         "<td>"+elementoEmpleado.sueldo+"</td>" +
+                         "</tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
